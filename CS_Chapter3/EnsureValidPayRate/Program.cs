da81d2d5526d49e19e4ae21151fcdd6792a923de
RJ Santos <rjs4nt0s@yahoo.com>
2015-10-27 11:32:47 -0400

    changed more

--- CS_Chapter3/EnsureValidPayRate/Program.cs
+++ CS_Chapter3/EnsureValidPayRate/Program.cs
@@ -10,13 +10,14 @@ namespace EnsureValidPayRate
         static void Main(string[] args)
         {
             string input, input2, num, num2;
-            double pay, number, number2, number3, weekly;
+            double pay, number, number2, number3, weekly, weekly2;
             Console.WriteLine("Input a hourly pay rate.");
             input = Console.ReadLine();
             pay = Convert.ToDouble(input);
 
             weekly = pay * 40;
             num = weekly.ToString("C");
+            
             number2 = 49.99;
             num2 = number2.ToString("C");
             number = 5.65;
@@ -26,13 +27,16 @@ namespace EnsureValidPayRate
                 Console.WriteLine("Enter number again.");
                 input2 = Console.ReadLine();
                 number3 = Convert.ToDouble(input2);
+                weekly2 = number3 * 40;
                 if (number3 < number || number3 > number2)
                 {
                     Console.WriteLine("Error.");
                 }
+                else
+                Console.WriteLine("Hourly pay rate is {0} and weekly pay rate is {1}", number3, weekly2);
             }
             else
-                Console.WriteLine("Hourly pay rate is {0} and weekly pay rate is {1}", num, num2);
+                Console.WriteLine("Hourly pay rate is {0} and weekly pay rate is {1}", pay, num);
         }
     }
 }
