1b5d712b41bce98549ab02cd8fb3c9a0f9bf0065
RJ Santos <rjs4nt0s@yahoo.com>
2015-10-29 11:22:34 -0400

    finished 4

--- CS_Chapter3/EnsureValidPayRate/Program.cs
+++ CS_Chapter3/EnsureValidPayRate/Program.cs
@@ -9,34 +9,35 @@ namespace EnsureValidPayRate
     {
         static void Main(string[] args)
         {
-            string input, input2, num, num2;
-            double pay, number, number2, number3, weekly, weekly2;
+            string input, input2, hour, week, hour2, week2;
+            double pay, low, high, pay2, weekly, weekly2;
             Console.WriteLine("Input a hourly pay rate.");
             input = Console.ReadLine();
             pay = Convert.ToDouble(input);
-
+            hour = pay.ToString("C");
             weekly = pay * 40;
-            num = weekly.ToString("C");
+            week = weekly.ToString("C");
             
-            number2 = 49.99;
-            num2 = number2.ToString("C");
-            number = 5.65;
+            high = 49.99;
+            low = 5.65;
 
-            if (pay < number || pay > number2)
+            if (pay < low || pay > high)
             {
                 Console.WriteLine("Enter number again.");
                 input2 = Console.ReadLine();
-                number3 = Convert.ToDouble(input2);
-                weekly2 = number3 * 40;
-                if (number3 < number || number3 > number2)
+                pay2 = Convert.ToDouble(input2);
+                weekly2 = pay2 * 40;
+                hour2 = pay2.ToString("C");
+                week2 = weekly2.ToString("C");
+                if (pay2 < low || pay2 > high)
                 {
                     Console.WriteLine("Error.");
                 }
                 else
-                Console.WriteLine("Hourly pay rate is {0} and weekly pay rate is {1}", number3, weekly2);
+                Console.WriteLine("Hourly pay rate is {0} and weekly pay rate is {1}", hour2, week2);
             }
             else
-                Console.WriteLine("Hourly pay rate is {0} and weekly pay rate is {1}", pay, num);
+                Console.WriteLine("Hourly pay rate is {0} and weekly pay rate is {1}", hour, week);
         }
     }
 }
--- CS_Chapter3/Furniture/Program.cs
+++ CS_Chapter3/Furniture/Program.cs
@@ -9,6 +9,39 @@ namespace Furniture
     {
         static void Main(string[] args)
         {
+            string input, pricep, priceo, pricem, incorrect;
+            double something, pine, oak, mahogany;
+            pine = 100;
+            pricep= pine.ToString("C");
+            oak = 225;
+            priceo= oak.ToString("C");
+            mahogany = 310;
+            pricem = mahogany.ToString("C");
+            something = 0;
+            incorrect = something.ToString("C");
+
+            Console.WriteLine("Pine wood = {0}", pricep);
+            Console.WriteLine("Oak wood = {0}", priceo);
+            Console.WriteLine("Mahogany wood = {0}", pricem);
+            Console.WriteLine("Choose 'P' for pine, 'O' for oak, or 'M' for mahogany.");
+
+            input = Console.ReadLine();
+            if (input == "p")
+            {
+                Console.WriteLine("You've selected pine wood for {0}.",pricep);
+            }
+            if (input == "o")
+            {
+                Console.WriteLine("You've selected oak wood for {0}.", priceo);
+            }
+            if (input == "m")
+            {
+                Console.WriteLine("You've selected mohagony wood for {0}.", pricem);
+            }
+            else
+            {
+                Console.WriteLine(incorrect);
+            }
         }
     }
 }
