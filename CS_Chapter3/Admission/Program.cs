84fab12bd5e00b597c15399313668784b38bba47
RJ Santos <rjs4nt0s@yahoo.com>
2015-11-02 11:02:06 -0500

    we done boys

--- CS_Chapter3/Admission/Program.cs
+++ CS_Chapter3/Admission/Program.cs
@@ -9,6 +9,20 @@ namespace Admission
     {
         static void Main(string[] args)
         {
+            double input, input2;
+            
+            Console.WriteLine("Enter your GPA.");
+            input = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("Enter your test score.");
+            input2 = Convert.ToDouble(Console.ReadLine());
+
+            if (input >= 3.0 && input2 >= 60)
+                Console.WriteLine("Accept");
+            else if (input < 3.0 && input2 >= 80)
+                Console.WriteLine("Accept");
+            else
+                Console.WriteLine("Reject");
         }
     }
 }
--- CS_Chapter3/Payroll/Program.cs
+++ CS_Chapter3/Payroll/Program.cs
@@ -9,6 +9,21 @@ namespace Payroll
     {
         static void Main(string[] args)
         {
+            string n;
+            double pay, hours, gross, withhold=0, net;
+
+            Console.WriteLine("Enter your hourly pay rate.");
+            pay = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter your hours worked.");
+            hours = Convert.ToDouble(Console.ReadLine());
+            gross = hours * pay;
+            if (pay <= 300.0)
+                withhold = .1;
+            else if (pay > 300.0)
+                withhold = .12;
+            net = gross - (gross * withhold);
+            n = net.ToString("C");
+            Console.WriteLine("Your net pay is {0}.", n);
         }
     }
 }
